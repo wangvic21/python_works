ca1d7a22c57b290864c34c01c4c126dbf39e42da
wangvic21 <wangvic21@gmail.com>
2018-09-02 14:11:03 +0800

    chapter 16.

---
+++ chapter_16/highs_lows.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv
+
+filename = 'sitka_weather_07-2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
